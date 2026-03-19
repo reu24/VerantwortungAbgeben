e752eb289addd1b92ada15714f3023d8ad768671
Alina Bürge <alina@mino-online.ch>
2026-03-19 14:03:04 +0100

    Add texts, images and improve design

--- src/public/index.js
+++ src/public/index.js
@@ -20,3 +20,34 @@ document.getElementById("submit-button").addEventListener("click", event => {
 document.getElementById('input-text').addEventListener('keyup', event => {
     document.getElementById("submit-button").disabled = document.getElementById("input-text").value.trim() === "";
 });
+
+
+// https://www.w3schools.com/W3CSS/tryit.asp?filename=tryw3css_slideshow_dots2
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        // @ts-ignore
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    // @ts-ignore
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-white";
+}
